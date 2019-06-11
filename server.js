622bbc2d56c68def9636c19b39e86559df0fb15c
Guilherme Sequeira <guilherme.a.sequeira@accenture.com>
2019-06-10 22:34:47 -0300

    rename app folder

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/firebase-angular-admin'));
+app.use(express.static(__dirname + '/dist/admin-v2'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/firebase-angular-admin/index.html'));
+res.sendFile(path.join(__dirname+'/dist/admin-v2/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
